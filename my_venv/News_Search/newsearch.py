4deb8ca021491b88ec555aa37201dc8d2a4fde18
nsk28 <nsk28@duke.edu>
2020-11-06 21:51:31 -0500

    push

--- my_venv/News_Search/newsearch.py
+++ my_venv/News_Search/newsearch.py
@@ -1,91 +1,95 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from newsapi import NewsApiClient
 import requests
 
 
 app = Flask(__name__)
+newsapi = NewsApiClient(api_key = "4f25baa11f644f33b5ea71ce1475df5e")
 
 #renders the home page 
 @app.route("/")
 def homepage():
-
     return render_template('gen_layout.html')
+
+@app.route("/", methods=['POST'])
+def homepage_search():
+    #get the input value in the search text box
+    topic = request.form['text'].split(" ")
+    mylist = search(topic)
+    return render_template('gen_layout.html', context = mylist)
 
 @app.route("/tech")
 def techpage():
-    newsapi = NewsApiClient(api_key = "4f25baa11f644f33b5ea71ce1475df5e")
+    mylist = search(["technology"])
 
-    tech_url = ("https://newsapi.org/v2/everything?q=technology&sortBy=popularity&apiKey=4f25baa11f644f33b5ea71ce1475df5e")
+    return render_template('gen_layout.html', context = mylist)
 
-    response = requests.get(tech_url).json()
-        
-    articles = response['articles']
-
-    desc=[]
-    news=[]
-    img = []
-
-    for i in range(len(articles)):
-        mine = articles[i]
-
-        news.append(mine['title'])
-        desc.append(mine['description'])
-        img.append(mine['urlToImage'])
-
-    mylist = zip(news, desc, img)
-
-
+@app.route("/tech", methods=['POST'])
+def techpage_search():
+    topic = request.form['text'].split(" ")
+    topic.append("technology")
+    mylist = search(topic)
     return render_template('gen_layout.html', context = mylist)
 
 @app.route("/entertainment")
 def entertainmentpage():
-    newsapi = NewsApiClient(api_key = "4f25baa11f644f33b5ea71ce1475df5e")
-    enter_url= ("https://newsapi.org/v2/everything?q=entertainment&sortBy=popularity&apiKey=4f25baa11f644f33b5ea71ce1475df5e")
+    mylist = search(["entertainment"])
+    return render_template('gen_layout.html', context = mylist)
 
-    response = requests.get(enter_url).json()
-        
-    articles = response['articles']
-
-    desc=[]
-    news=[]
-    img = []
-
-    for i in range(len(articles)):
-        mine = articles[i]
-
-        news.append(mine['title'])
-        desc.append(mine['description'])
-        img.append(mine['urlToImage'])
-
-    mylist = zip(news, desc, img)
-
-
+@app.route("/entertainment", methods=['POST'])
+def entertainment_search():
+    topic = request.form['text'].split(" ")
+    topic.append("entertainment")
+    mylist = search(topic)
     return render_template('gen_layout.html', context = mylist)
 
 @app.route("/sports")
 def sportspage():
-    newsapi = NewsApiClient(api_key = "4f25baa11f644f33b5ea71ce1475df5e")
-    sport_url= ("https://newsapi.org/v2/everything?q=sports&sortBy=popularity&apiKey=4f25baa11f644f33b5ea71ce1475df5e")
+    mylist = search(["sports"])
+    return render_template('gen_layout.html', context = mylist)
 
-    response = requests.get(sport_url).json()
+@app.route("/sports", methods=['POST'])
+def sports_search():
+    topic = request.form['text'].split(" ")
+    topic.append("sports")
+    mylist = search(topic)
+    return render_template('gen_layout.html', context = mylist)
+
+def search(input):
+    search_url = ("https://newsapi.org/v2/everything?q=" + " OR ".join(input) + "&sortBy=popularity&apiKey=4f25baa11f644f33b5ea71ce1475df5e")
+
+    response = requests.get(search_url).json()
         
     articles = response['articles']
 
-    desc=[]
-    news=[]
-    img = []
+    desc_odd=[]
+    news_odd=[]
+    img_odd = []
+    url_odd = []
+
+    desc_even=[]
+    news_even=[]
+    img_even=[]
+    url_even=[]
 
     for i in range(len(articles)):
         mine = articles[i]
 
-        news.append(mine['title'])
-        desc.append(mine['description'])
-        img.append(mine['urlToImage'])
+        if(i%2 == 1):
+            news_odd.append(mine['title'])
+            desc_odd.append(mine['description'])
+            img_odd.append(mine['urlToImage'])
+            url_odd.append(mine['url'])
+        else:
+            news_even.append(mine['title'])
+            desc_even.append(mine['description'])
+            img_even.append(mine['urlToImage'])
+            url_even.append(mine['url'])
 
-    mylist = zip(news, desc, img)
+    
+    mylist = zip(news_odd, desc_odd, img_odd, url_odd, news_even, desc_even, img_even, url_even)
 
-
-    return render_template('gen_layout.html', context = mylist)
+    return mylist
 
 if __name__== "__main__":
     app.run(debug=True)
